8849b1bcf61551598072bcd934d02239b81b72ba
sleepyv <32480250+sleepyv@users.noreply.github.com>
2018-02-28 10:17:40 -0800

    Will finish rest of error checking for fs_mount later

--- libfs/fs.c
+++ libfs/fs.c
@@ -6,13 +6,77 @@
 #include "disk.h"
 #include "fs.h"
 
+#define FAT_EOC 0xFFFF
+int bytes_to_block(int y);
+typedef struct __attribute__((__packed__)) sBlock{
+	
+	char Sig[8];
+	uint16_t tNumBlocks;
+	uint16_t rdb_Index;
+	uint16_t BlockStart;
+	uint16_t nDataBlocks;
+	uint8_t  nFAT_Blocks;
+	char padding[4079];
+}
+
+typedef struct __attribute__((__packed__)) FAT{
+	
+	uint16_t *f_table;//will dynamically allocate, = nDataBlocks
+}
+typedef struct __attribute__((__packed__)) Root_Dir{
+	
+	char Filename[FS_FILENAME_LEN];
+	uint32_t fSize;
+	uint16_t  index;
+	char padding[10];
+}
+int FS_Mount=0;
+typedef struct sBlock * SB;
+typedef struct Root_Dir * RD;
+typedef struct FAT * fat;
 /* TODO: Phase 1 */
 
 int fs_mount(const char *diskname)
 {
+	int cmp, retVal;
+	char signature[9] = {'E','C','S','1','5','0','F','S','\0'};
+	SB = malloc(sizeof(sBlock));
+	RD = malloc(FS_FILE_MAX_COUNT*sizeof(Root_Dir));
+	
+	if(block_read(0, (void*)SB)!=0){
+		free(SB);
+		free(RD);
+		return -1;
+	}
+	SB = (sBlock*)SB;
+	
+	if(strcmp(signature, SB.Sig)!=0){
+		free(SB);
+		free(RD);
+		return -1;
+	}
+	if(block_disk_count()!=SB.tNumBlocks){
+		free(SB);
+		free(RD);
+		return -1;
+	}
+	fat->f_table =  malloc(SB->nDataBlocks *sizeof(FAT));
+	fat->f_table[0]= FAT_EOC;
+	
+	FS_Mount=1;
+	return 0;
+	
 	/* TODO: Phase 1 */
 }
 
+//for section 4
+int bytes_to_block(int y){
+	int i=0;
+	while((BLOCK_SIZE*i)<y){	
+		i++;
+	}
+	return i;
+}
 int fs_umount(void)
 {
 	/* TODO: Phase 1 */
@@ -20,6 +84,15 @@ int fs_umount(void)
 
 int fs_info(void)
 {
+	if(FS_Mount==0)return -1;
+	
+	fprintf(stderr,"File System Info: ");
+	fprintf(stderr,"Signature: %s",SB.Sig);
+	fprintf(stderr,"Number of Total Number of Blocks: %d",SB.NumBlocks);//not sure if sould use %d
+	fprintf(stderr,"Number of Datablocks: %d",SB.nDataBlocks);
+	fprintf(stderr,"Number of FAT Blocks: %d",SB.nFAT_Blocks);
+	
+	return 0;
 	/* TODO: Phase 1 */
 }
 
