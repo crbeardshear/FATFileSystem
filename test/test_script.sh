21fa0483a5af54d224ca8cecf41285079d84e4b2
Colin <crbeardshear@ucdavis.edu>
2018-03-07 20:20:06 -0800

    updated test script

--- test/test_script.sh
+++ test/test_script.sh
@@ -4,7 +4,8 @@
 # compares outputs of reference program and our library
 cmp_output()
 {
-    echo "Test $testnum: $1"
+    echo ""
+    echo "Test $testnum: $1 $2 $3 $4"
     echo "Comparison for test $testnum:"
 
     # put output files into variables
@@ -39,8 +40,8 @@ cmp_output()
 # Start of script:
 
 # make fresh virtual disk, separate ones for fs_ref.x and test_fs.x
-./fs_make.x refdisk.fs 2048
-./fs_make.x libdisk.fs 2048
+./fs_make.x refdisk.fs 50
+./fs_make.x libdisk.fs 50
 # initialize variables
 testnum=1
 
@@ -60,21 +61,106 @@ cmp_output info
 num=0
 while [ $num -lt 1000 ]
 do
-    echo "test" >> ln5000
+    echo "test" >> test1
     num=$(($num+1))
 done
 
-./fs_ref.x add refdisk.fs ln5000 >ref.stdout 2>ref.stderr
-./test_fs.x add libdisk.fs ln5000 >lib.stdout 2>lib.stderr
+./fs_ref.x add refdisk.fs test1 >ref.stdout 2>ref.stderr
+./test_fs.x add libdisk.fs test1 >lib.stdout 2>lib.stderr
 
 # compare fs_ref.x to test_fs.x
-cmp_output write5000
+cmp_output write test1 5,000 bytes
 
-rm ln5000
+rm test1
 
-#
-# unfinished!
-#
+#add length 50000
+num=0
+while [ $num -lt 10000 ]
+do
+    echo "test" >> test2
+    num=$(($num+1))
+done
+
+./fs_ref.x add refdisk.fs test2 >ref.stdout 2>ref.stderr
+./test_fs.x add libdisk.fs test2 >lib.stdout 2>lib.stderr
+
+# compare fs_ref.x to test_fs.x
+cmp_output write test2 50,000 bytes
+
+rm test2
+
+# ls the file system, should have the two files we just wrote
+./fs_ref.x ls refdisk.fs >ref.stdout 2>ref.stderr
+./test_fs.x ls libdisk.fs >lib.stdout 2>lib.stderr
+cmp_output ls
+
+# read the test2 we wrote
+./fs_ref.x cat refdisk.fs test2 >ref.stdout 2>ref.stderr
+./test_fs.x cat libdisk.fs test2 >lib.stdout 2>lib.stderr
+cmp_output cat test2
+
+# stat the test2
+./fs_ref.x stat refdisk.fs test2 >ref.stdout 2>ref.stderr
+./test_fs.x stat libdisk.fs test2 >lib.stdout 2>lib.stderr
+cmp_output stat test2
+
+# info after the two files were added
+./fs_ref.x info refdisk.fs >ref.stdout 2>ref.stderr
+./test_fs.x info libdisk.fs >lib.stdout 2>lib.stderr
+cmp_output info after adds
+
+# add file over size of the file system (it must be truncated to fit)
+# the string is 23 characters (with end of line char)
+# times 10000 is 230,000 bytes
+# file system is 50 blocks * 4096 = 204,800 bytes
+num=0
+while [ $num -lt 10000 ]
+do
+    echo "test number 3 asdf asdf" >> test3
+    num=$(($num+1))
+done
+
+./fs_ref.x add refdisk.fs test3 >ref.stdout 2>ref.stderr
+./test_fs.x add libdisk.fs test3 >lib.stdout 2>lib.stderr\
+
+cmp_output write test3 230,000 bytes
+rm test3
+
+# stat the test3
+./fs_ref.x stat refdisk.fs test3 >ref.stdout 2>ref.stderr
+./test_fs.x stat libdisk.fs test3 >lib.stdout 2>lib.stderr
+cmp_output stat test3
+
+# add file with conflicting name
+echo "test" > test1
+./fs_ref.x add refdisk.fs test1 >ref.stdout 2>ref.stderr
+./test_fs.x add libdisk.fs test1 >lib.stdout 2>lib.stderr
+cmp_output add test1 conflict
+
+# remove one of the files (test1)
+./fs_ref.x rm refdisk.fs test1 >ref.stdout 2>ref.stderr
+./test_fs.x rm libdisk.fs test1 >lib.stdout 2>lib.stderr
+cmp_output rm test1
+
+# ls after the removal to see if the file was removed correctly
+./fs_ref.x ls refdisk.fs >ref.stdout 2>ref.stderr
+./test_fs.x ls libdisk.fs >lib.stdout 2>lib.stderr
+cmp_output ls after rm
+
+# info after conflict
+./fs_ref.x info refdisk.fs >ref.stdout 2>ref.stderr
+./test_fs.x info libdisk.fs >lib.stdout 2>lib.stderr
+cmp_output info after conflict
+
+# remove one of the files (test2)
+./fs_ref.x rm refdisk.fs test2 >ref.stdout 2>ref.stderr
+./test_fs.x rm libdisk.fs test2 >lib.stdout 2>lib.stderr
+cmp_output rm test2
+
+# remove last of the files (test3)
+./fs_ref.x rm refdisk.fs test3 >ref.stdout 2>ref.stderr
+./test_fs.x rm libdisk.fs test3 >lib.stdout 2>lib.stderr
+cmp_output rm test3
 
 # clean
 rm refdisk.fs libdisk.fs
